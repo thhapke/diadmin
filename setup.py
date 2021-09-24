b474c24b4a32333525017b35c54d2a4c4cc78822
thhapke <thorsten.hapke@sap.com>
2021-09-24 04:26:58 +0200

    update

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="diadmin",
-    version="0.0.21",
+    version="0.0.22",
     author="Thorsten Hapke",
     author_email="thorsten.hapke@sap.com",
     description="Utility scripts for SAP Data Intelligence policy management",
