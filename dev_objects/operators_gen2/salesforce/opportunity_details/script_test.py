b9e2681e3cc14e5a5437db124a073ed4a7f9ba6a
thhapke <thorsten.hapke@sap.com>
2022-12-13 12:37:19 +0100

    update

--- dev_objects/operators_gen2/salesforce/opportunity_details/script_test.py
+++ dev_objects/operators_gen2/salesforce/opportunity_details/script_test.py
@@ -12,18 +12,17 @@ api.init(__file__)     # class instance of mock_api
 
 
 api.config.httpconnection_basic = { 'connectionProperties':
-                           {'host': 'https://api-staging.pwc.com/',
-                            'user': 'DE_ifs_Integration-Platform_s001',
-                            'password': 'D0uV%/r(I^!_>Q}gK1Eo',
-                            'path': '/de-ip_temp'
+                           {'host': '',
+                            'user': '',
+                            'password': '',
+                            'path': 'p'
                             }
                        }
 
 api.config.httpconnection_details = { 'connectionProperties':
                            {'host': 'https://api-staging.pwc.com/',
-                            'user': '3d05882e-6f47-40b6-a1f5-dc6ff6197dd0 0832861272c45caee336459ff49cd582 '\
-                            'l712dedd2e8d504e21ab1f2e89988a64ab aba40940b5244c1ebc1e02f7c176ae26',
-                            'password': 'Basic REVfaWZzX0ludGVncmF0aW9uLVBsYXRmb3JtX3MwMDE6RDB1ViUvcihJXiFfPlF9Z0sxRW8=',
+                            'user': '',
+                            'password': '',
                             'path': '/de-ip_temp'
                             }
                        }
